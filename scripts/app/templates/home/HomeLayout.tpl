4069b58d82464cf2584545164cff843c0f017ad2
Mohammad Hussain <khatreemohd@gmail.com>
2013-10-28 11:55:54 +0530

    Dummy Data integration
    Dummy Data integration

--- scripts/app/templates/home/HomeLayout.tpl
+++ scripts/app/templates/home/HomeLayout.tpl
@@ -8,29 +8,62 @@
     <div id="scroller">
       <ul id="thelist">
         <li class="img">
-          {@embed:Placeholder {
-          name : "header"
-         }/}
+         <div class="images"><span class="title">amadeus | <span class="titleName">Travel Now</span></span></div>
         </li>
         <li class="headerTxt"> Inspirations </li>
-        <li>{@embed:Placeholder {
-          name : "content"
-         }/}
-         </li>
+        <li>
+          <span class="facebook" {on click { fn : facebookData} /}>Friends</span>
+          <span class="seasonal" {on click { fn : seasonalData} /}>Seasonal</span>
+          <span class="wellBeing" {on click { fn : wellbeingData} /}>Well being</span>
+        </li>
       </ul>
     </div>
   </div>
   <div id="footer">
-    {@embed:Placeholder {
-      name : "footer"
-    }/}
+    <div class="button">About amadeus</div>
   </div>
   <div class="popUp">
-  {@embed:Placeholder {
-      name : "popup"
-    }/}
+    {call popup() /}
   </div>
   <div class="mask"></div>
+  <div class="loading"><img src="img/ajaxloader.gif"></div>
+  {/macro}
+
+{macro popup()}
+  <div class="dialog">
+    {section {
+        id : "popuplist",
+        macro : {
+          name: "popupmacro"
+        },bindRefreshTo : [{
+          to : "popupData",
+          inside : data
+        }]
+    }/}
+  </div>
+  {/macro}
+  {macro popupmacro()}
+    {if this.data.popupData }
+    <div class="header">${this.data.popupData.title}<span class="close"></span></div>
+    <div class="popUpText">${this.data.popupData.desc}</div>
+    <div class="faceBookPopUp">
+    {if this.data.popupData.list.length > 0}
+      {set lists = this.data.popupData.list /}
+      ${console.log(lists)|eat}
+      {foreach list inArray lists}
+        <div class="button">
+          <ul>
+            <li>${list.title}</li>
+            <li class="smallFont">${list.desc}</li>
+            <li class="icon"></li>
+          </ul>
+        </div>
+        {/foreach}
+       {else/}
+        Sorry! Could not found entries...
+      {/if}
+    </div>
+    {/if}
   {/macro}
 
 {/Template}
--- scripts/app/templates/home/HomeLayoutScript.js
+++ scripts/app/templates/home/HomeLayoutScript.js
@@ -6,9 +6,57 @@ Aria.tplScriptDefinition({
 	$prototype : {
 
         $displayReady: function(){
+        	$(".mask, .dialog, .popUp").hide();
+			
+			$(".close").click(function(){
+				$(".mask, .dialog, .popUp").hide();
+			});			
+
 			myScroll = new iScroll('wrapper');
 			document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 			document.addEventListener('DOMContentLoaded', function () { setTimeout(loaded, 200); }, false);
+		},
+		facebookData: function(){
+			var myData = {};
+			myData.title = "Inspired by friends";
+			myData.desc = "The top three locations where several of your Facebook friends have recently enjoyed visiting, and we’re offering you great deals and offers to try it yourself";
+			myData.list = new Array();
+			myData.list[0] = {};
+			myData.list[0].title = "Paris, France";
+			myData.list[0].desc = "visitied by 15 of your friends";
+			myData.list[1] = {};
+			myData.list[1].title = "New Delhi,, India";
+			myData.list[1].desc = "visitied by 25 of your friends";
+			this.$json.setValue(this.data,"popupData",myData);
+
+			$(".loading, .mask").show();
+			$(".loading").hide();
+			$(".popUp, .dialog").show();
+		},
+		seasonalData: function(){
+			var myData = {};
+			myData.title = "Seasonal inspirations";
+			myData.desc = "We know it is the time of the year, when you have an opportunity to take a break from your hectic life and escape to an exotic or tranquil places. Here is a great list of offers for such places to visit.";
+			myData.list = new Array();
+			//myData.list[0] = {};
+			this.$json.setValue(this.data,"popupData",myData);
+
+			$(".loading, .mask").show();
+			$(".loading").hide();
+			$(".popUp, .dialog").show();
+		},
+		wellbeingData: function(){
+			var myData = {};
+			myData.title = "Well being inspirations";
+			myData.desc = "It seems you are likely to seek holidays with a specific focus, for example, wellbeing/medical tourism, learning/cultural holidays and ethical voyages. Here is a list of such great occasions which you would like to consider.";
+			myData.list = new Array();
+			//myData.list[0] = {};
+			this.$json.setValue(this.data,"popupData",myData);
+
+			$(".loading, .mask").show();
+			$(".loading").hide();
+			$(".popUp, .dialog").show();
+			$(".mask, .popUp, .dialog").show();
 		}
 
     }
